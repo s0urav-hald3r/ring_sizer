dc40cb8a7bbea64e5b9f6ae9442d3984a48478a3
s0urav-hald3r <bishalder96@gmail.com>
2024-11-15 01:28:57 +0530

    constants file added

---
+++ lib/config/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color primaryColor = Color(0xFFFAEFE6);
+const Color secondaryColor = Color(0xFF79593D);
+
+// Icons
+const String twoDIcon = 'assets/icons/2DIcon.svg';
+const String threeDIcon = 'assets/icons/3DIcon.svg';
+const String addIcon = 'assets/icons/addIcon.svg';
+const String backIcon = 'assets/icons/backIcon.svg';
+const String convertIcon = 'assets/icons/convertIcon.svg';
+const String crossIcon = 'assets/icons/crossIcon.svg';
+const String infoIcon = 'assets/icons/infoIcon.svg';
+const String menuIcon = 'assets/icons/menuIcon.svg';
+const String minusIcon = 'assets/icons/minusIcon.svg';
+const String navConverterIcon = 'assets/icons/navConverterIcon.svg';
+const String navFingerIcon = 'assets/icons/navFingerIcon.svg';
+const String navRingIcon = 'assets/icons/navRingIcon.svg';
+const String saveIcon = 'assets/icons/saveIcon.svg';
+const String settingsIcon = 'assets/icons/settingsIcon.svg';
+
+// Images
+const String cardHolder = 'assets/images/cardHolder.png';
+const String diamondRing = 'assets/images/diamondRing.png';
+const String plainRing = 'assets/images/plainRing.png';
+const String premiumPage = 'assets/images/premiumPage.png';
+const String onboardingOne = 'assets/images/onboardingOne.png';
